8e32a6669823033b31b4b5da8f5e3d3101d3df59
snizhanakovalchukkb2025-dot <snizhana.kovalchuk.kb.2025@lpnu.ua>
2026-09-18 11:55:09 +0300

    Create lib.py
    Add lib.py module

---
+++ lib.py
@@ -0,0 +1,15 @@
+# lib.py - Модуль із математичними функціями
+
+def multiply(a, b):
+    """Повертає добуток двох чисел."""
+    return a * b
+
+
+def power(base, exponent):
+    """Підносить число base до степеня exponent."""
+    return base ** exponent
+
+
+def subtract(a, b):
+    """Повертає різницю двох чисел (a - b)."""
+    return a - b
